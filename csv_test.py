bccf7c1a5992df552c4a31e3755b695726d43da9
Stephen <cheunste@gmail.com>
2020-06-17 11:11:18 -0700

    Initial commit

--- csv_test.py
+++ csv_test.py
@@ -2,10 +2,19 @@ import unittest
 import os
 import csv
 import main
+from pathlib import Path
 
 
-class MyTestCase(unittest.TestCase):
+class CsvTestClass(unittest.TestCase):
 	csv_file_path = r"./csvFile.csv"
+
+	def get_testing_line_from_csv_file(self):
+		with open(self.csv_file_path, newline='') as csvFile:
+			csvFileReader = csv.reader(csvFile, delimiter=',')
+			csvFileReader.__next__()
+			line = next(csvFileReader)
+		return line
+
 	def test_csv_file_exists(self):
 		csv_file_exists = os.path.isfile(self.csv_file_path)
 		self.assertTrue(csv_file_exists)
@@ -20,29 +29,53 @@ class MyTestCase(unittest.TestCase):
 
 	def test_contain_correct_headers(self):
 		required_headers = [r"site abbreviation",r"Number of Turbines at Site",r"UTC Offset",r"Turbine Backup",r"Latitude",r"Longtitude"]
-		required_headers.sort()
 		line =[]
 		with open(self.csv_file_path,newline='') as csvFile:
 			csvFileReader = csv.reader(csvFile,delimiter=',')
 			line = next(csvFileReader)
-		line.sort()
 		self.assertTrue(line == required_headers,"The CSV file does not contained the required headers")
 		self.assertTrue(len(line) == len(required_headers),"The number of items do not match")
 
 	def test_empty_field(self):
 		line=self.get_testing_line_from_csv_file()
-		line.sort()
-		self.assertTrue(main.is_line_contains_empty_fcell(line))
+		self.assertTrue(main.is_line_contains_empty_cell(line))
 
-	def test_valid_lat_long(self):
-		line = self.get_testing_line_from_csv_file()
+	def test_create_map(self):
+		header_line = main.get_name_of_header_fields()
+		dummy_test_list = []
+		for i in range (0, len(header_line)):
+			dummy_test_list.append(f"Test{i}")
+		csv_map = main.create_csv_map(header_line,dummy_test_list)
+		self.assertTrue(any(header in csv_map for header in header_line))
+		self.assertTrue(any(values in csv_map.values() for values in dummy_test_list))
 
-	def get_testing_line_from_csv_file(self):
-		with open(self.csv_file_path, newline='') as csvFile:
-			csvFileReader = csv.reader(csvFile, delimiter=',')
-			csvFileReader.__next__()
-			line = next(csvFileReader)
-		return line
+class DatabaseTestClass(unittest.TestCase):
+	csv_file_path = r"./csvFile.csv"
+	configuration_db_path = r".\ZubatConfiguration.db"
+
+	def test_configuration_DB_exists(self):
+		fileExists = main.configuration_db_exists()
+		self.assertTrue(fileExists)
+
+	def test_database_connection(self):
+		self.assertIsNotNone(main.connect_to_database())
+
+	def test_read_from_sql(self):
+		number_of_turbines = main.get_number_of_turbines_in_database()
+		self.assertTrue(number_of_turbines == 19)
+
+	def test_insert_to_db(self):
+		query = f"INSERT into TurbineInputTags VALUES ('Test','Test','Test','Test','Test','Test','Test','Test','Test','Test')"
+		main.execute_database_query(query)
+		number_of_turbines = main.get_number_of_turbines_in_database()
+		self.assertTrue(number_of_turbines >= 19)
+
+	def test_remove_from_db(self):
+		query = f"Delete from TurbineInputTags WHERE TurbineId=\"Test\""
+		main.execute_database_query(query)
+		number_of_turbines = main.get_number_of_turbines_in_database()
+		self.assertTrue(number_of_turbines == 19)
+
 
 if __name__ == '__main__':
 	unittest.main()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import csv
+import sqlite3
 
+from pathlib import Path
+
+configuration_db_path=".\ZubatConfiguration.db"
 
 def check_csv_file_header():
 	csv_file_path = r"./csvFile.csv"
@@ -16,6 +20,45 @@ def get_number_of_fields_in_csv_file():
 		line = csvFileReader.__next__()
 	return len(line)
 
-def is_line_contains_empty_fcell(line):
+def get_name_of_header_fields():
+	csv_file_path = r"./csvFile.csv"
+	with open(csv_file_path, newline='') as csvFile:
+		csvFileReader = csv.reader(csvFile, delimiter=',')
+		line = csvFileReader.__next__()
+	return line
+
+def is_line_contains_empty_cell(line):
 	return any('' in s for s in line)
+
+
+def configuration_db_exists(configuration_db_path=".\ZubatConfiguration.db"):
+	configuration_file = Path(configuration_db_path)
+	if configuration_file.is_file():
+		return True
+	else:
+		return False
+
+def connect_to_database(configuration_db_path=".\ZubatConfiguration.db"):
+	return sqlite3.connect(configuration_db_path)
+
+def get_number_of_turbines_in_database(configuration_db_path=".\ZubatConfiguration.db"):
+	return connect_to_database().cursor().execute("select count(DISTINCT TurbineId) from TurbineOutputTags").fetchone()[0]
+
+def execute_database_query(query):
+	conn = connect_to_database()
+	c = conn.cursor()
+	c.execute(query)
+	conn.commit()
+	return
+
+def create_csv_map(header_row, csv_row):
+	return dict(zip(header_row,csv_row))
+
+def main():
+	return
+
+
+if __name__ == "main":
+	main()
+
 
